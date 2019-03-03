c927c6d5e3a422f5e52ba4dd06ac609105f2acd3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-03 18:19:08 +0000

    Deploy devgg/gh-issues to github.com/devgg/gh-issues.git:gh-pages

---
+++ precache-manifest.186827a54645cea82ba53f8059b9bf93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2affcb455dfaefa3e23d",
+    "url": "/issues/static/css/main.1585d15a.chunk.css"
+  },
+  {
+    "revision": "2affcb455dfaefa3e23d",
+    "url": "/issues/static/js/main.2affcb45.chunk.js"
+  },
+  {
+    "revision": "002c6ea5a78b3c7ae964",
+    "url": "/issues/static/js/runtime~main.002c6ea5.js"
+  },
+  {
+    "revision": "cf46bf967cbe98a9b460",
+    "url": "/issues/static/js/2.cf46bf96.chunk.js"
+  },
+  {
+    "revision": "4049b6ad9800addf94418fa7d8fe2c13",
+    "url": "/issues/index.html"
+  }
+];
