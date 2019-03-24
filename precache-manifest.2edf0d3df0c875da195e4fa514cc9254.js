ec5126cbac9eb8585460952cd1f1f7b4e84085cf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-24 18:00:59 +0000

    Deploy devgg/gh-issues to github.com/devgg/gh-issues.git:gh-pages

---
+++ precache-manifest.2edf0d3df0c875da195e4fa514cc9254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0a10be289b2a110e17c",
+    "url": "/contri/static/css/main.d8f1ec21.chunk.css"
+  },
+  {
+    "revision": "a0a10be289b2a110e17c",
+    "url": "/contri/static/js/main.a0a10be2.chunk.js"
+  },
+  {
+    "revision": "9783c31a0f5fd17d8a07",
+    "url": "/contri/static/js/runtime~main.9783c31a.js"
+  },
+  {
+    "revision": "64618fd3d17303c5b0b1",
+    "url": "/contri/static/js/2.64618fd3.chunk.js"
+  },
+  {
+    "revision": "e085116d84a0bd2abb763a579ae6dc39",
+    "url": "/contri/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gh-issues/precache-manifest.fd56085ca579f2e474fae2f95d96bc61.js"
+  "/contri/precache-manifest.2edf0d3df0c875da195e4fa514cc9254.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/gh-issues/index.html", {
+workbox.routing.registerNavigationRoute("/contri/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
