451b14b94c738aebda42855c1498d4ce280ee833
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-16 08:13:08 +0000

    Deploy devgg/contrib to github.com/devgg/contrib.git:gh-pages

--- precache-manifest.c4b7a59858e4be45ae1d39e2d437acf9.js
+++ precache-manifest.c4b7a59858e4be45ae1d39e2d437acf9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "1e1957cdc06064783f17",
+    "revision": "dd1ee3eb50ac8fe84590",
     "url": "/contrib/static/css/main.a79c4400.chunk.css"
   },
   {
-    "revision": "1e1957cdc06064783f17",
-    "url": "/contrib/static/js/main.1e1957cd.chunk.js"
+    "revision": "dd1ee3eb50ac8fe84590",
+    "url": "/contrib/static/js/main.dd1ee3eb.chunk.js"
   },
   {
     "revision": "452d1390efa3e135611a",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/contrib/static/js/2.2250e550.chunk.js"
   },
   {
-    "revision": "86657ba2f98c4b50e1384161c07becf8",
+    "revision": "976a3dd21f0132ad767294cd690b968a",
     "url": "/contrib/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/contrib/precache-manifest.7ec8ddb6957a6e3c479d2e20a1940d13.js"
+  "/contrib/precache-manifest.c4b7a59858e4be45ae1d39e2d437acf9.js"
 );
 
 workbox.clientsClaim();
