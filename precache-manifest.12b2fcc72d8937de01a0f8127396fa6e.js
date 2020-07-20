c8cc6d4c5c4378ef13b0943165f514727e4fb2e0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-20 08:52:05 +0000

    Deploy devgg/contrib to github.com/devgg/contrib.git:gh-pages

--- precache-manifest.12b2fcc72d8937de01a0f8127396fa6e.js
+++ precache-manifest.12b2fcc72d8937de01a0f8127396fa6e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "8bda6e4e82c42cdd23d9",
+    "revision": "f1bc16472433163620fe",
     "url": "/contrib/static/css/main.a79c4400.chunk.css"
   },
   {
-    "revision": "8bda6e4e82c42cdd23d9",
-    "url": "/contrib/static/js/main.8bda6e4e.chunk.js"
+    "revision": "f1bc16472433163620fe",
+    "url": "/contrib/static/js/main.f1bc1647.chunk.js"
   },
   {
     "revision": "452d1390efa3e135611a",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/contrib/static/js/2.2250e550.chunk.js"
   },
   {
-    "revision": "d0373556662ab9f049fa729e415b790d",
+    "revision": "c1fbcc337ea9e233b71e3a4267c6d7f1",
     "url": "/contrib/index.html"
   }
 ];
