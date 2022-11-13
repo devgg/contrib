bd771a313b197c8f52f7c105a2ee82be93eed7af
devgg <devgg@users.noreply.github.com>
2022-11-13 06:53:40 +0000

    Deploying to gh-pages from @ devgg/contrib@725c134179eac52cc2f0a0406365aa5caf6645ee 🚀

--- precache-manifest.966044cf1b0b592a68b0fb13c699d4a4.js
+++ precache-manifest.966044cf1b0b592a68b0fb13c699d4a4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "23e700bb5e4512ff1108",
+    "revision": "9baa75ac6cbf475ba0b8",
     "url": "/contrib/static/css/main.6b245962.chunk.css"
   },
   {
-    "revision": "23e700bb5e4512ff1108",
-    "url": "/contrib/static/js/main.23e700bb.chunk.js"
+    "revision": "9baa75ac6cbf475ba0b8",
+    "url": "/contrib/static/js/main.9baa75ac.chunk.js"
   },
   {
     "revision": "6d57b6e49674b37de67b",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/contrib/static/js/2.e1127459.chunk.js"
   },
   {
-    "revision": "46a1deb15b4f6e886b023abb15c35b18",
+    "revision": "6ce5871266b696da8f687f0028ca49fc",
     "url": "/contrib/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/contrib/precache-manifest.c91d5eafd61b9776fe9955992e1c6218.js"
+  "/contrib/precache-manifest.966044cf1b0b592a68b0fb13c699d4a4.js"
 );
 
 workbox.clientsClaim();
